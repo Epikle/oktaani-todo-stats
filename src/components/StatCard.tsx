c90daff006514d8c397de3f5ecf70fbc5c162a2d
Epikle <98042337+Epikle@users.noreply.github.com>
2023-09-06 17:25:04 +0300

    :chart_with_upwards_trend: Add 6 months data

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -1,9 +1,10 @@
-import { Bar, BarChart, ResponsiveContainer } from 'recharts';
+import { Bar, BarChart, Cell, ResponsiveContainer } from 'recharts';
 
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/Card';
@@ -17,18 +18,22 @@ interface StatCardProps {
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'percent',
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
   signDisplay: 'exceptZero',
 });
 
 export function StatCard({ title, description, unit, data }: StatCardProps) {
-  const changeFromLastMonth =
-    data.length > 1
-      ? formatter.format(
-          (data.at(-1)!.point - data.at(-2)!.point) / data.at(-2)!.point
-        )
-      : null;
+  const previousDataPoint = data.at(-2)?.point || 0;
+  const currentDataPoint = data.at(-1)?.point || 0;
+
+  const changeFromLastMonth = formatter.format(
+    previousDataPoint !== 0
+      ? (currentDataPoint - previousDataPoint) / previousDataPoint
+      : currentDataPoint !== 0
+      ? 1
+      : 0
+  );
 
   return (
     <Card>
@@ -36,7 +41,7 @@ export function StatCard({ title, description, unit, data }: StatCardProps) {
         <CardTitle className="text-base">{title}</CardTitle>
         <CardDescription>{description}</CardDescription>
       </CardHeader>
-      <CardContent className="pb-2">
+      <CardContent className="pb-0">
         <div className="flex items-center justify-center space-x-2">
           <div className="flex-1 text-center">
             <div className="text-5xl font-bold tracking-tighter">
@@ -47,7 +52,7 @@ export function StatCard({ title, description, unit, data }: StatCardProps) {
             </div>
             {changeFromLastMonth && (
               <div className="text-sm text-muted-foreground mt-2">
-                {changeFromLastMonth} from last month
+                <b>{changeFromLastMonth}</b> from last month
               </div>
             )}
           </div>
@@ -56,12 +61,30 @@ export function StatCard({ title, description, unit, data }: StatCardProps) {
           <div className="my-3 h-[60px]">
             <ResponsiveContainer width="100%" height="100%">
               <BarChart data={data}>
-                <Bar dataKey="point" />
+                <Bar dataKey="point">
+                  {data.map((entry, index) =>
+                    entry.point ? (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={index === data.length - 1 ? '#991b1b' : 'black'}
+                      />
+                    ) : (
+                      <Cell
+                        key={`cell-${index}`}
+                        height={1}
+                        fill={index === data.length - 1 ? '#991b1b' : 'gray'}
+                      />
+                    )
+                  )}
+                </Bar>
               </BarChart>
             </ResponsiveContainer>
           </div>
         )}
       </CardContent>
+      <CardFooter className="text-sm text-muted-foreground justify-center">
+        last 6 months
+      </CardFooter>
     </Card>
   );
 }
